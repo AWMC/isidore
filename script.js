4f03d0b58d0081128967e8d281a2045494210a0b
gracebell29 <111467576+gracebell29@users.noreply.github.com>
2023-11-12 18:47:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -179,7 +179,6 @@ document.addEventListener("DOMContentLoaded", function () {
     map.on('click', function (event) {
         const feature = map.forEachFeatureAtPixel(event.pixel, (feature) => feature);
         if (feature) {
-            url: 'points.geojson',
             const properties = feature.getProperties();
             const content = `
                 <ul>
@@ -191,14 +190,7 @@ document.addEventListener("DOMContentLoaded", function () {
             popupContent.innerHTML = content;
             overlay.setPosition(event.coordinate);
 
-            else {
-            url: 'lines.geojson',
-            const properties = feature.getProperties();
-            const content = `
-                <ul>
-                    <li><strong>Region:</strong> ${properties.Region}</li>
-                </ul>
-            `;
+           
             popupContent.innerHTML = content;
             overlay.setPosition(event.coordinate);
             if (selectedFeature) {
